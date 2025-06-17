23bc866d9628e9cacbbd75ac9a6d4133c4d0d08a
Tạ Đình Tiến <95206365+tienclay@users.noreply.github.com>
2025-06-17 05:53:09 +0000

    solve simple problem

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,15 @@ import './App.css';
 function App() {
   const name = 'Clay';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <h1>Hello, {name}</h1>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <img src="" alt="" className="feature-image" />
+      <h3 className="feature-title">Title</h3>
+      <p className="feature-desc">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores
+        ipsam at, consequuntur repellendus itaque, odio, perferendis consequatur
+        quo officiis rem ducimus doloribus nulla ex? Possimus expedita
+        asperiores odit a consectetur.
+      </p>
     </div>
   );
 }
